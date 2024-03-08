06dc13344c575e00595475eb25cba1edce2b350b
chayex18 <133992144+chayex18@users.noreply.github.com>
2024-03-08 17:36:35 -0500

    Update P3.py

--- P3.py
+++ P3.py
@@ -17,29 +17,63 @@ def printGraph():
         else:
             nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.9, style='dashed')
     plt.show(block = False)
-    input("Press enter to continue")
+    input("Press enter to continue ")
 
 def isTriangle(graph, weight):
-    global i 
-    i = 0
     for u, v in graph.edges():
         if graph[u][v]['weight'] == weight:
             for w in graph.neighbors(v):
                 if graph.has_edge(u, w) and graph[u][w]['weight'] == weight:
                     if graph.has_edge(w, u) and graph[w][u]['weight'] == weight:
                         if graph[u][v]['weight'] == graph[v][w]['weight'] == graph[w][u]['weight']:
-                            i = 1
+                            #print("Triangle found: ", u, v, w)
                             return True
     return False
 
 def player1():
+    plt.close()
     edge1 = int(input("Enter First Edge: ")) 
     edge2 = int(input("Enter Second Edge: "))
+    while isCondition(edge1, edge2) is False:
+        edge1 = int(input("Enter First Edge: ")) 
+        edge2 = int(input("Enter Second Edge: "))
     graph.add_edge(edge1,edge2, weight = 1)
     printGraph()
+    if isTriangle(graph, 1):
+        print("Player 1 Won")
+        return 0
+    else:
+        plt.close()
+        player2()
+
+def isCondition(edge1, edge2):
+    if ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):
+        print("Please choose a node between 1 and 6")
+        return False
+    elif (edge1 == edge2):
+        print("You can't create a line to the same node")
+        return False
+    elif (graph.has_edge(edge1, edge2)):
+        print("Please select a different line that is not taken")
+        return False
+    else:
+        return True
 
 def player2():
     edge1 = int(input("Enter First Edge: ")) 
     edge2 = int(input("Enter Second Edge: "))
+    while isCondition(edge1, edge2) is False:
+        edge1 = int(input("Enter First Edge: ")) 
+        edge2 = int(input("Enter Second Edge: "))
     graph.add_edge(edge1,edge2, weight = 0)
     printGraph()
+    if isTriangle(graph, 0):
+        print("Player 2 Won")
+        return 0
+    else:
+        plt.close()
+        player1()
+
+buildGraph()
+printGraph()
+player1()
