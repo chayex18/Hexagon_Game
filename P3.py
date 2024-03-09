66e479e6f75602c68dcd0410e8a17661d391a63e
chayex18 <133992144+chayex18@users.noreply.github.com>
2024-03-09 11:01:50 -0500

    Update P3.py

--- P3.py
+++ P3.py
@@ -1,141 +1,139 @@
-"""
-Hexagon Game with Minimax Algorithm
-This Hexagon game's implementation, two players take turns adding edges to a hexagonal graph.
-The main goal is to avoid creating triangles with the added edges. 
-The AI player (Player 2) uses the minimax algorithm to make optimal moves.
+# Yeixon Chacon U:24830068
+# Elias Hurtando U:00042456
+# This program written in python is an Hexagonal Game played against an AI that follows the minimax algorithm with a depth of 3.
+# The player will be prompted if he/she wants to play first or second by inputting the number 1 or 2:
+# - If select to play as player 1 the program makes the first move.
+# - If select to play as player 2 the user makes the first move.
 
-"""
-import networkx as nx  # Importing NetworkX library for graph manipulation
-import matplotlib.pyplot as plt # Importing Matplotlib library for graph visualization
+import networkx as nx               # Use of networkx to create graph used in game
+import matplotlib.pyplot as plt     # Use of matplotlib.pyplot to visually show the game and the current state
 
-# Initialize the hexagonal graph
-def buildGraph():
-    global graph 
-    graph = nx.Graph()  # Creating an empty graph
-    graph.add_nodes_from([1, 2, 3, 4, 5, 6])   # Adding nodes to the graph
-    global pos 
-    pos = {1: (0, 1), 2: (1, 2), 3: (2, 1), 4: (2, -1), 5: (1, -2), 6: (0, -1)}
-    
-# Printing the graph with current state
-def printGraph():
-    # Drawing the graph with labels and node properties
-    nx.draw(graph,pos, with_labels=True, node_color='green', node_size=500, font_size=12)
-    # Drawing edges with different styles based on weight
-    for u, v, d in graph.edges(data=True):
-        if d['weight'] == 1:
-            nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.5) # Solid edges
-        else:
-            nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.9, style='dashed') # Dashed edges
-    plt.show(block = False)  # Displaying the graph without blocking the program execution
-    input("Press enter to continue ") # Waiting for user input to continue
+def buildGraph():       # Function to build the graph
+    global graph        # Declare graph as global to be used in the whole program
+    graph = nx.Graph()  # Build graph using networkx
+    graph.add_nodes_from([1, 2, 3, 4, 5, 6])    # Add six nodes for the game
+    global pos      # Create pos to represent position of nodes as global
+    pos = {1: (0, 1), 2: (1, 2), 3: (2, 1), 4: (2, -1), 5: (1, -2), 6: (0, -1)} # Set position of nodes
 
-# Checking if a triangle is formed in the graph
-def isTriangle(graph, weight):
-    for u, v in graph.edges():
-        if graph[u][v]['weight'] == weight:
-            for w in graph.neighbors(v):
+def printGraph():   # Function to print the Graph
+    plt.close()     # Close any visualization if opened 
+    nx.draw(graph,pos, with_labels=True, node_color='green', node_size=500, font_size=12)   # Draw the nodes with specific characteristics
+    for u, v, d in graph.edges(data=True):  # For edge drawing
+        if playerNum == 2:  # If player number input is 2
+            if d['weight'] == 1:    # The user will control the solid line
+                nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.5)
+            else:                   # And the AI will be presented as the dashed line
+                nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.9, style='dashed')
+        else:   # Else if the player input is 1
+            if d['weight'] == 1:    # The user will control de dashed line 
+                nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.9, style='dashed')
+            else:                   # And the AI will be presented as the solid line
+                nx.draw_networkx_edges(graph, pos, edgelist=[(u, v)], width=4.0, alpha=0.5)
+    plt.show(block = False) # Show visual representation of graph drawned. Used block = false to make program keep running after graph is displayed
+
+def isTriangle(graph, weight):  # Function to check if a triangle was made in the graph
+    for u, v in graph.edges():  # Check for all edges in graph
+        if graph[u][v]['weight'] == weight: # If the edges have the weight desired
+            for w in graph.neighbors(v):    # Check for its neighbors and repeat process:
                 if graph.has_edge(u, w) and graph[u][w]['weight'] == weight:
                     if graph.has_edge(w, u) and graph[w][u]['weight'] == weight:
                         if graph[u][v]['weight'] == graph[v][w]['weight'] == graph[w][u]['weight']:
-                            return True
-    return False
-    
-# Validate edge selection conditions
-def isCondition(edge1, edge2):
-    if ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):
-        print("Please choose a node between 1 and 6")
-        return False
-    elif (edge1 == edge2):
-        print("You can't create a line to the same node")
-        return False
-    elif (graph.has_edge(edge1, edge2)):
-        print("Please select a different line that is not taken")
-        return False
-    else:
-        return True
-        
-# Validate edge selection conditions for minimax algorithm    
-def isCondition2(edge1, edge2):
-    if ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):
-        return False
-    elif (edge1 == edge2):
-        return False
-    elif (graph.has_edge(edge1, edge2)):
-        return False
-    else:
-        return True
-        
-# Player 1's turn
-def player1():
-    printGraph()
-    edge1 = int(input("Enter First Edge: ")) 
-    edge2 = int(input("Enter Second Edge: "))
-    while isCondition(edge1, edge2) is False:
-        edge1 = int(input("Enter First Edge: ")) 
-        edge2 = int(input("Enter Second Edge: "))
-    plt.close()
-    graph.add_edge(edge1,edge2, weight = 1)
-    printGraph()
-    if isTriangle(graph, 1):
-        print("Player 1 Created a Triangle. Player 1 Lost!")
-        plt.show()
-        return 0
-    else:
-        plt.close()
-        player2()
+                            return True # If all three neighbors are the same return True, which means a triangle was formed
+    return False    # Else return false
 
-# Player 2's turn using minimax algorithm
-def player2():
-    # Use minimax algorithm to choose the best move for player 2
-    _, edge1, edge2 = minimax(graph, 0, 2, True)
-    graph.add_edge(edge1, edge2, weight=0)
-    printGraph()
-    if isTriangle(graph, 0):
-        print("Player 2 Created a Triangle. Player 2 Lost!")
-        plt.show()
-        return 0
+def isCondition(edge1, edge2):  # Validate edge selection conditions
+    if (edge1 == None):
+        return False
+    elif ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):    
+        print("Please choose a node between 1 and 6")   # Print message for users
+        return False
+    elif (edge1 == edge2):
+        print("You can't create a line to the same node")   # Print message for users
+        return False
+    elif (graph.has_edge(edge1, edge2)):
+        print("Please select a different line that is not taken")   # Print message for users
+        return False
     else:
-        plt.close()
-        player1()
-# Evaluating the game state 
-def evaluate_game_state(graph):
+        return True # If conditions passed return True
+    
+def isCondition2(edge1, edge2): # Validate edge selection conditions for minimax algorithm. This will not print any message
+    if ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):
+        return False
+    elif (edge1 == edge2):
+        return False
+    elif (graph.has_edge(edge1, edge2)):
+        return False
+    else:
+        return True
+
+# Player's 1 Turn (User)
+def player1():
+    printGraph()    # Print Graph
+    # Set edges to None
+    edge1 = None 
+    edge2 = None 
+    while isCondition(edge1, edge2) is False:   # Until conditions are not met
+        edge1 = int(input("Enter First Edge: "))    # Print to enter First Edge
+        edge2 = int(input("Enter Second Edge: "))   # Print to enter Second Edge
+    graph.add_edge(edge1,edge2, weight = 1) # Add them to the graph
+    printGraph()    # Print the graph
+    if isTriangle(graph, 1):    # Check if change made a triangle
+        print(f"Player {playerNum2} Created a Triangle. Player {playerNum2} Lost!")    # In case it did show message that player lost the game
+        plt.show()  # Show final state of game
+        return 0    # Finish Program
+    else:   # If game has not finished
+        input("Press enter to continue ")   # Enter to continue will allow user to control when to go to the next state
+        plt.close() # Close window
+        player2()   # Go to player two
+
+def player2():  # Player 2's turn using minimax algorithm
+    _, edge1, edge2 = minimax(graph, 0, True)   # Return the most accurate edge1 and edge2
+    graph.add_edge(edge1, edge2, weight=0)  # Add it to the graph
+    printGraph()    
+    if isTriangle(graph, 0):    # If triangle was made
+        print(f"Player {playerNum} Created a Triangle. Player {playerNum} Lost!")   # show message that player lost the game
+        plt.show()  # Show final state of game
+        return 0    # Finish Program
+    else:   # If game has not finished
+        plt.close() # Close window
+        player1()   # Go to player one
+
+def evaluate_game_state(graph): # Evaluating the game state 
     if isTriangle(graph, 1):  # If player 1 forms a triangle, return a winning score
         return 1
-    elif isTriangle(graph, 0):  # If player 2 forms a triangle, return a loosing score
+    elif isTriangle(graph, 0):  # If player 2 (AI) forms a triangle, return a loosing score
         return -1
     else:
         return 0  # Game is ongoing 
 
-# Implementing minimax algorithm for AI player
-
-def minimax(graph, depth, player, is_maximizing_player):
-    if depth == 3 or isTriangle(graph, 0) or isTriangle(graph, 1):  # Depth limit reached, evaluate the game state
-        return evaluate_game_state(graph), None, None
+def minimax(graph, depth, is_maximizing_player): # Function with minimax algorithm
+    if depth == 3 or isTriangle(graph, 0) or isTriangle(graph, 1):  # If Depth limit reached or a triangle is found evaluate the game state
+        return evaluate_game_state(graph), None, None   # Return the game state - The other nones represent edges that are not used here.
     
-    if is_maximizing_player:
-        best_value = -10000 # Set toa large value (represents negative infinity)
+    if is_maximizing_player:    # If looking for mac
+        best_value = float('-inf') # Set to a large value (represents negative infinity)
         best_edge1 = None
         best_edge2 = None
-        for edge1 in range(1, 7):
+        for edge1 in range(1, 7):   # Look for all possible combinations
             for edge2 in range(edge1 + 1, 7):
-                if isCondition2(edge1, edge2):
-                    graph.add_edge(edge1, edge2, weight = 0 )
-                    value, _, _ = minimax(graph, depth + 1, player, False)
-                    if value > best_value:
-                        best_value = value
+                if isCondition2(edge1, edge2):  # If conditions are met
+                    graph.add_edge(edge1, edge2, weight = 0 )   # Add values to edge
+                    value, _, _ = minimax(graph, depth + 1, False)  # Call minimax to return value of nodes
+                    if value > best_value:  # If value is grater than best value (for first iteration negative infinite)
+                        best_value = value  
                         best_edge1 = edge1
                         best_edge2 = edge2
                     graph.remove_edge(edge1, edge2)                    
         return best_value, best_edge1, best_edge2
     else:
-        best_value = 10000
+        best_value = float('inf')
         best_edge1 = None
         best_edge2 = None
         for edge1 in range(1, 7):
             for edge2 in range(edge1 + 1, 7):
                 if isCondition2(edge1, edge2):
                     graph.add_edge(edge1, edge2, weight = 0)
-                    value, _, _ = minimax(graph, depth + 1, player, True)
+                    value, _, _ = minimax(graph, depth + 1, True)
                     if value < best_value:
                         best_value = value
                         best_edge1 = edge1
@@ -156,10 +154,17 @@ def getPrompt():
         else:
             return prompt
 
-# Start the game based on the user's input
-playerNum = getPrompt()
-buildGraph()
-if playerNum == 2:
-    player1()
-else:
-    player2()
+def main():
+    global playerNum
+    global playerNum2
+    playerNum = getPrompt()
+    buildGraph()
+    if playerNum == 2:
+        playerNum2 = 1
+        player1()
+    else:
+        playerNum2 = 2
+        player2()
+
+if __name__ == "__main__":
+    main()
