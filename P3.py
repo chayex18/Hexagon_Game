13432b0f2369b009bf3f765489c5cabb9c4d9132
chayex18 <133992144+chayex18@users.noreply.github.com>
2024-03-09 08:28:53 -0500

    Update P3.py

--- P3.py
+++ P3.py
@@ -8,7 +8,6 @@ def buildGraph():
     global pos 
     pos = {1: (0, 1), 2: (1, 2), 3: (2, 1), 4: (2, -1), 5: (1, -2), 6: (0, -1)}
 
-
 def printGraph():
     nx.draw(graph,pos, with_labels=True, node_color='green', node_size=500, font_size=12)
     for u, v, d in graph.edges(data=True):
@@ -26,25 +25,8 @@ def isTriangle(graph, weight):
                 if graph.has_edge(u, w) and graph[u][w]['weight'] == weight:
                     if graph.has_edge(w, u) and graph[w][u]['weight'] == weight:
                         if graph[u][v]['weight'] == graph[v][w]['weight'] == graph[w][u]['weight']:
-                            #print("Triangle found: ", u, v, w)
                             return True
     return False
-
-def player1():
-    plt.close()
-    edge1 = int(input("Enter First Edge: ")) 
-    edge2 = int(input("Enter Second Edge: "))
-    while isCondition(edge1, edge2) is False:
-        edge1 = int(input("Enter First Edge: ")) 
-        edge2 = int(input("Enter Second Edge: "))
-    graph.add_edge(edge1,edge2, weight = 1)
-    printGraph()
-    if isTriangle(graph, 1):
-        print("Player 1 Won")
-        return 0
-    else:
-        plt.close()
-        player2()
 
 def isCondition(edge1, edge2):
     if ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):
@@ -58,22 +40,110 @@ def isCondition(edge1, edge2):
         return False
     else:
         return True
+    
+def isCondition2(edge1, edge2):
+    if ((edge1 < 1) or (edge1 > 6) or (edge2 < 1) or (edge2 > 6)):
+        return False
+    elif (edge1 == edge2):
+        return False
+    elif (graph.has_edge(edge1, edge2)):
+        return False
+    else:
+        return True
 
-def player2():
+def player1():
+    printGraph()
     edge1 = int(input("Enter First Edge: ")) 
     edge2 = int(input("Enter Second Edge: "))
     while isCondition(edge1, edge2) is False:
         edge1 = int(input("Enter First Edge: ")) 
         edge2 = int(input("Enter Second Edge: "))
-    graph.add_edge(edge1,edge2, weight = 0)
+    plt.close()
+    graph.add_edge(edge1,edge2, weight = 1)
+    printGraph()
+    if isTriangle(graph, 1):
+        print("Player 1 Created a Triangle. Player 1 Lost!")
+        plt.show()
+        return 0
+    else:
+        plt.close()
+        player2()
+
+def player2():
+    # Use minimax algorithm to choose the best move for player 2
+    _, edge1, edge2 = minimax(graph, 0, 2, True)
+    graph.add_edge(edge1, edge2, weight=0)
     printGraph()
     if isTriangle(graph, 0):
-        print("Player 2 Won")
+        print("Player 2 Created a Triangle. Player 2 Lost!")
+        plt.show()
         return 0
     else:
         plt.close()
         player1()
+###
 
+def evaluate_game_state(graph):
+    if isTriangle(graph, 1):  # If player 1 forms a triangle, return a winning score
+        return 1
+    elif isTriangle(graph, 0):  # If player 2 forms a triangle, return a loosing score
+        return -1
+    else:
+        return 0  # Game is ongoing 
+
+# Implement minimax algorithm here
+
+def minimax(graph, depth, player, is_maximizing_player):
+    if depth == 3 or isTriangle(graph, 0) or isTriangle(graph, 1):  # Depth limit reached, evaluate the game state
+        return evaluate_game_state(graph), None, None
+    
+    if is_maximizing_player:
+        best_value = -10000 # Set toa large value (represents negative infinity)
+        best_edge1 = None
+        best_edge2 = None
+        for edge1 in range(1, 7):
+            for edge2 in range(edge1 + 1, 7):
+                if isCondition2(edge1, edge2):
+                    graph.add_edge(edge1, edge2, weight = 0 )
+                    value, _, _ = minimax(graph, depth + 1, player, False)
+                    if value > best_value:
+                        best_value = value
+                        best_edge1 = edge1
+                        best_edge2 = edge2
+                    graph.remove_edge(edge1, edge2)                    
+        return best_value, best_edge1, best_edge2
+    else:
+        best_value = 10000
+        best_edge1 = None
+        best_edge2 = None
+        for edge1 in range(1, 7):
+            for edge2 in range(edge1 + 1, 7):
+                if isCondition2(edge1, edge2):
+                    graph.add_edge(edge1, edge2, weight = 0)
+                    value, _, _ = minimax(graph, depth + 1, player, True)
+                    if value < best_value:
+                        best_value = value
+                        best_edge1 = edge1
+                        best_edge2 = edge2
+                    graph.remove_edge(edge1, edge2)
+        return best_value, best_edge1, best_edge2
+
+def getPrompt():
+    while True:
+        try:
+            prompt = int(input("Please enter a player number (1 or 2): "))
+            if prompt < 1 or prompt > 2:
+                print("Please select either 1 or 2")
+                continue
+        except ValueError:
+            print("Invalid input. Enter an integer (either 1 or 2)")
+        else:
+            return prompt
+
+
+playerNum = getPrompt()
 buildGraph()
-printGraph()
-player1()
+if playerNum == 2:
+    player1()
+else:
+    player2()
